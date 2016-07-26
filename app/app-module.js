6941e100d05a4f23a8415c0e9ead7cf06c264a67
Varun Goel <varun.goel@optimusinfo.com>
2016-07-26 20:19:03 +0530

    Added initial files

---
+++ app/app-module.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name sumanAngularApp
+   * @description
+   *
+   */
+  angular
+    .module('sumanAngularApp', [
+      'ui.router',
+      'ui.bootstrap',
+      'home'
+    ]);
+}());
