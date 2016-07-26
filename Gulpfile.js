b8ed9adf1519a33256a3af4a1e6e40bcd5022ea7
Varun Goel <varun.goel@optimusinfo.com>
2016-07-26 21:59:46 +0530

    Initial code of smartadmin seed project

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,59 +1,102 @@
-'use strict';
+var es = require('event-stream');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var templateCache = require('gulp-angular-templatecache');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var fs = require('fs');
+var _ = require('lodash');
 
-var _ = require('lodash')
-  , buildConfig = require('./build.config')
-  , config = {}
-  , gulp = require('gulp')
-  , gulpFiles = require('require-dir')('./gulp')
-  , path = require('path')
-  , $, key;
 
-$ = require('gulp-load-plugins')({
-  pattern: [
-  'browser-sync',
-  'del',
-  'gulp-*',
-  'karma',
-  'main-bower-files',
-  'multi-glob',
-  'plato',
-  'run-sequence',
-  'streamqueue',
-  'uglify-save-license',
-  'wiredep',
-  'yargs'
-  ]
+var scripts = require('./app.scripts.json');
+
+var source = {
+    js: {
+        main: 'app/main.js',
+        src: [
+            // application config
+            'app.config.js',
+
+            // application bootstrap file
+            'app/main.js',
+
+            // main module
+            'app/app.js',
+
+            // module files
+            'app/**/module.js',
+
+            // other js files [controllers, services, etc.]
+            'app/**/!(module)*.js'
+        ],
+        tpl: 'app/**/*.tpl.html'
+    }
+};
+
+var destinations = {
+    js: 'build'
+};
+
+
+gulp.task('build', function(){
+    return es.merge(gulp.src(source.js.src) , getTemplateStream())
+         .pipe(ngAnnotate())
+         .pipe(uglify())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(destinations.js));
 });
 
-_.merge(config, buildConfig);
-
-config.appFiles = path.join(config.appDir, '**/*');
-config.appFontFiles = path.join(config.appDir, 'fonts/**/*');
-config.appImageFiles = path.join(config.appDir, 'images/**/*');
-config.appMarkupFiles = path.join(config.appDir, '**/*.html');
-config.appScriptFiles = path.join(config.appDir, '**/*.js');
-config.appStyleFiles = path.join(config.appDir, '**/*.css');
-
-config.buildDirectiveTemplateFiles = path.join(config.buildDir, '**/*directive.tpl.html');
-config.buildJsFiles = path.join(config.buildJs, '**/*.js');
-
-config.buildTestDirectiveTemplateFiles = path.join(config.buildTestDir, '**/*directive.tpl.html');
-config.buildE2eTestsDir = path.join(config.buildTestDir, 'e2e');
-config.buildE2eTests = path.join(config.buildE2eTestsDir, '**/*_test.js');
-config.buildTestDirectiveTemplatesDir = path.join(config.buildTestDir, 'templates');
-config.buildUnitTestsDir = path.join(config.buildTestDir, config.unitTestDir);
-config.buildUnitTestFiles = path.join(config.buildUnitTestsDir, '**/*_test.js');
-
-config.e2eFiles = path.join('e2e', '**/*.js');
-config.unitTestFiles = path.join(config.unitTestDir, '**/*_test.js');
-
-for (key in gulpFiles) {
-  gulpFiles[key](gulp, $, config);
-}
-
-gulp.task('dev', ['build'], function () {
-  gulp.start('browserSync');
-  gulp.start('watch');
+gulp.task('js', function(){
+    return es.merge(gulp.src(source.js.src) , getTemplateStream())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(destinations.js));
 });
 
+gulp.task('watch', function(){
+    gulp.watch(source.js.src, ['js']);
+    gulp.watch(source.js.tpl, ['js']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        port: 8888
+    });
+});
+
+gulp.task('vendor', function(){
+    _.forIn(scripts.chunks, function(chunkScripts, chunkName){
+        var paths = [];
+        chunkScripts.forEach(function(script){
+            var scriptFileName = scripts.paths[script];
+
+            if (!fs.existsSync(__dirname + '/' + scriptFileName)) {
+
+                throw console.error('Required path doesn\'t exist: ' + __dirname + '/' + scriptFileName, script)
+            }
+            paths.push(scriptFileName);
+        });
+        gulp.src(paths)
+            .pipe(concat(chunkName + '.js'))
+            //.on('error', swallowError)
+            .pipe(gulp.dest(destinations.js))
+    })
+
+});
+
+gulp.task('prod', ['vendor', 'build']);
+gulp.task('dev', ['vendor', 'js', 'watch', 'connect']);
 gulp.task('default', ['dev']);
+
+var swallowError = function(error){
+    console.log(error.toString());
+    this.emit('end')
+};
+
+var getTemplateStream = function () {
+    return gulp.src(source.js.tpl)
+        .pipe(templateCache({
+            root: 'app/',
+            module: 'app'
+        }))
+};
